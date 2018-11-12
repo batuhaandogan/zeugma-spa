f0fcec228e632e9a02b83888e0d41f072821f637
batuhaandogan <tipala_1909@hotmail.com>
2018-11-12 18:19:58 -0500

    navbar

--- myfolder/src/components/Home.js
+++ myfolder/src/components/Home.js
@@ -8,6 +8,9 @@ class Home extends Component {
     render() {
         return (
        <div>
+           <head>
+           <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet" />
+           </head>
                 <div className="home-page">
                     <h1>The home page</h1>
                 </div>
