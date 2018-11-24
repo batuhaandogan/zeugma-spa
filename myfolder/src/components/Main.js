8fcf60376c38692ea53d45910f103be82e338acb
batuhaandogan <tipala_1909@hotmail.com>
2018-11-23 22:38:47 -0500

    fixed the boxes

--- myfolder/src/components/Main.js
+++ myfolder/src/components/Main.js
@@ -11,7 +11,7 @@ class Main extends Component {
     render() {
         return (
             
-            <div>
+            <div className="main-class-for">
                   <head>
                          <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet" />
                     </head>
