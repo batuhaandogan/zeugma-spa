7e65e7599edfb13ff3ac54f52acc9cadb2602e2c
batuhaandogan <tipala_1909@hotmail.com>
2018-11-23 23:20:56 -0500

    adding stuffs

--- myfolder/src/components/Main.js
+++ myfolder/src/components/Main.js
@@ -81,9 +81,9 @@ class Main extends Component {
 <img id="images-id" src={bath} />
 <div className="massage-box">
     <h1>Massage</h1>
-    <p>Over time stress builds in our body and <br/>can 
-        leave us feeling run down. A relaxing<br/> massage
-         can help elevate this feeling of<br/> tension 
+    <p>Over time stress builds in our body and can 
+        leave us feeling run down. A relaxing massage
+         can help elevate this feeling of tension 
          and promote an overall sense of well being.</p>
 </div>
 
@@ -99,8 +99,8 @@ class Main extends Component {
 <img id="images-id" src={beauty} />
 <div className="massage-box">
     <h1>Beauty</h1>
-    <p>Spa beauty treatments are not only about<br/> relaxation.
-     An elaborate spa therapy session is<br/> also about
+    <p>Spa beauty treatments are not only about relaxation.
+     An elaborate spa therapy session is also about
       a holistic experience that  promotes healthy living.
          </p>
 </div>
@@ -116,10 +116,10 @@ class Main extends Component {
 <img id="images-id" src={amazing} />
 <div className="massage-box">
     <h1>Relaxation</h1>
-    <p>With the busy lives we lead it’s no<br/>
-     wonder we can sometimes find<br/>
-      ourselves feeling stressed. This is<br/>
-       why it’s essential to find a little<br/>
+    <p>With the busy lives we lead it’s no
+     wonder we can sometimes find
+      ourselves feeling stressed. This is
+       why it’s essential to find a little
         ‘me time’ in daily life to keep ourselves feeling good.
          </p>
 </div>
@@ -127,10 +127,24 @@ class Main extends Component {
 </div>
 
 
-
-                   
-                {/* </div> */}
                 </div>
+
+
+                    <div className="scroll-to-treatments">
+
+
+                     <div className="border-make">
+                    <h1>To see our Services</h1>
+
+                         <p>
+                                <a className="the-a-tag" href="home">Click Here</a>
+                                </p>
+                    </div>
+
+
+                    </div>
+
+
             </div>
         )
     }
